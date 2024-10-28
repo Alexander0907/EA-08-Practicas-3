e9bd1586a99758da78a980695faf8e287a6edddc
FelipeBACRC <afba.2599@outlook.com>
2024-10-28 15:25:10 -0600

    Se solucionan problemas con el SQL

--- src/main/java/com/practica1/domain/Arbol.java
+++ src/main/java/com/practica1/domain/Arbol.java
@@ -15,10 +15,15 @@ public class Arbol implements Serializable{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name ="id")
     private Long id;
+    @Column(name ="nombre_comun")
     private String nombreComun;
+    @Column(name ="tipo_flor")
     private String tipoFlor;
+    @Column(name ="dureza_madera")
     private String durezaMadera;
+    @Column(name ="altura_promedio")
     private float altura;
+    @Column(name ="ruta_imagen")
     private String rutaImagen;
 
     public Arbol(){
